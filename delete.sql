98b7109efaf0cba6d1139b6d186fd3c8d3291ea8
Enia Lahcene <136603666+enialahcene@users.noreply.github.com>
2025-03-16 17:35:49 +0100

    Update delete.sql

--- delete.sql
+++ delete.sql
@@ -1,9 +1,11 @@
 USE lab_mysql;
 
+-- Desactivar el modo seguro de actualizaciones para permitir DELETE sin una clave WHERE
 SET SQL_SAFE_UPDATES = 0;
 
+-- Eliminar la fila usando únicamente el identificador car_id
 DELETE FROM cars
-WHERE car_id = 4
-AND vin = 'DAM41UDN3CHU2WVF6';
+WHERE car_id = 4;
 
+-- Reactivar el modo seguro de actualizaciones
 SET SQL_SAFE_UPDATES = 1;
