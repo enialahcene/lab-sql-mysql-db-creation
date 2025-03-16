b74d8ed60eee4fc8b4bc422f121910e44b38c9f2
Enia Lahcene <136603666+enialahcene@users.noreply.github.com>
2025-03-16 17:39:58 +0100

    Update update.sql

--- update.sql
+++ update.sql
@@ -1,18 +1,23 @@
 -- update.sql
 USE lab_mysql;
 
+-- Desactivar el modo seguro de actualizaciones para permitir UPDATE sin una clave WHERE
 SET SQL_SAFE_UPDATES = 0;
 
+-- Actualizar el email de Pablo Picasso usando customer_id
 UPDATE customers
 SET email = 'ppicasso@gmail.com'
-WHERE name = 'Pablo Picasso';
+WHERE customer_id = '1001';  -- Usar customer_id en lugar del nombre
 
+-- Actualizar el email de Abraham Lincoln usando customer_id
 UPDATE customers
 SET email = 'lincoln@us.gov'
-WHERE name = 'Abraham Lincoln';
+WHERE customer_id = '2001';  -- Usar customer_id en lugar del nombre
 
+-- Actualizar el email de Napoléon Bonaparte usando customer_id
 UPDATE customers
 SET email = 'hello@napoleon.me'
-WHERE name = 'Napoléon Bonaparte';
+WHERE customer_id = '3001';  -- Usar customer_id en lugar del nombre
 
+-- Reactivar el modo seguro de actualizaciones
 SET SQL_SAFE_UPDATES = 1;
